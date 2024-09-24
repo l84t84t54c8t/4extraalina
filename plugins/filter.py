bf8c2d2f764fc693f9c8c858d87cf078921e0d03
l888lmn <deakoking85@gmail.com>
2024-09-24 13:46:25 +0300

    Update filter.py

--- plugins/filter.py
+++ plugins/filter.py
@@ -4,19 +4,19 @@ import re
 from config import BANNED_USERS
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from YukkiMusic import app
-from YukkiMusic.utils.database import (
+from AlinaMusic import app
+from AlinaMusic.utils.database import (
     deleteall_filters,
     get_filter,
     get_filters_names,
     save_filter,
 )
-from YukkiMusic.utils.functions import (
+from AlinaMusic.utils.functions import (
     check_format,
     extract_text_and_keyb,
     get_data_and_name,
 )
-from YukkiMusic.utils.keyboard import ikb
+from AlinaMusic.utils.keyboard import ikb
 
 from utils.error import capture_err
 from utils.permissions import adminsOnly, member_permissions
