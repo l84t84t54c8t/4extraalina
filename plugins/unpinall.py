4f4da6d557aac78b435e653836234c4fc93c704c
l888lmn <deakoking85@gmail.com>
2024-09-24 16:54:21 +0300

    Update unpinall.py

--- plugins/unpinall.py
+++ plugins/unpinall.py
@@ -1,6 +1,6 @@
 from pyrogram import enums, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from YukkiMusic import app
+from AlinaMusic import app
 
 
 @app.on_callback_query(filters.regex(r"^unpin"))
