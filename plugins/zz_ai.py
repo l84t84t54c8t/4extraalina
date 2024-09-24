2292fb2ab6addbd16793e339539799720a12e6af
l888lmn <deakoking85@gmail.com>
2024-09-24 13:41:37 +0300

    Update zz_ai.py

--- plugins/zz_ai.py
+++ plugins/zz_ai.py
@@ -5,7 +5,7 @@ import requests
 from MukeshAPI import api
 from pyrogram import filters
 from pyrogram.enums import ChatAction
-from YukkiMusic import app
+from AlinaMusic import app
 
 # List of supported emojis for reactions
 EMOJI_LIST = [
