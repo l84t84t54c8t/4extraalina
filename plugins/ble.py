5e0fbdee90a280d9a69e2f4f56f3d52eed3409d3
l888lmn <deakoking85@gmail.com>
2024-10-04 22:12:40 +0300

    Update ble.py

--- plugins/ble.py
+++ plugins/ble.py
@@ -3,13 +3,22 @@ from pyrogram import filters
 
 SLEEP = 0.1
 
-
 @app.on_message(filters.regex("^بڵێ|^بلی") & filters.group)
 async def say(app, message):
     if message.text.startswith("بلی") and message.reply_to_message:
-        txt = message.text.split(None, 1)[1]
-        return await message.reply_to_message.reply(txt)
+        # Split and check if there's additional text
+        split_text = message.text.split(None, 1)
+        if len(split_text) > 1:  # Ensure there's something to reply with
+            txt = split_text[1]
+            return await message.reply_to_message.reply(txt)
+        else:
+            return await message.reply("**- تکایە وشەم پێ بە بۆ دووبارەکردنەوە**")
 
     elif message.text.startswith("بڵێ"):
-        txt = message.text.split(None, 1)[1]
-        return await message.reply(txt)
+        # Split and check if there's additional text
+        split_text = message.text.split(None, 1)
+        if len(split_text) > 1:  # Ensure there's something to reply with
+            txt = split_text[1]
+            return await message.reply(txt)
+        else:
+            return await message.reply("**- تکایە وشەم پێ بە بۆ دووبارەکردنەوە**")
