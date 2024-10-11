ed887771f6dd61a65993e4c2c3d0cbdf16c1c9a4
l888lmn <deakoking85@gmail.com>
2024-10-11 12:11:13 +0300

    Create Forward.py

---
+++ plugins/Forward.py
@@ -0,0 +1,23 @@
+from pyrogram import filters
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.errors import MessageDeleteForbidden
+from AlinaMusic import app
+
+
+@app.on_message(filters.forwarded)
+async def gjgh(app, m):
+    # Ensure m.chat and m.from_user are not None
+    if m.chat is None or m.from_user is None:
+        return  # Exit the function if they are None
+
+    try:
+        chat_member = await app.get_chat_member(m.chat.id, m.from_user.id)
+        su = chat_member.status
+
+    # Check if the user's status is "member"
+    if su == ChatMemberStatus.MEMBER:
+            await m.delete()
+    except MessageDeleteForbidden:
+        print("Bot does not have permission to delete the message.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
