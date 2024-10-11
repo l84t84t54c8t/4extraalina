b09e969157e7709c563b7170feead178e3437806
l888lmn <deakoking85@gmail.com>
2024-10-11 11:52:36 +0300

    Update deleteforward.py

--- plugins/deleteforward.py
+++ plugins/deleteforward.py
@@ -114,21 +114,19 @@ async def delete_story(client, message):
 
 # Forwarded Message Deletion
 @app.on_message(filters.forwarded)
-async def delete_forwarded(app, m):
+async def gjgh(app, m):
+    # Ensure m.chat and m.from_user are not None
     if m.chat is None or m.from_user is None:
         return  # Exit the function if they are None
 
-    # Check if forwarded deletion is enabled
-    if not await is_deletion_enabled(m.chat.id, "forwarded"):
+    # Check if story deletion is enabled
+    if not await is_deletion_enabled(chat_id, "forwarded"):
         return
 
-    try:
-        chat_member = await app.get_chat_member(m.chat.id, m.from_user.id)
-        su = chat_member.status
+    # Fetch the chat member status
+    chat_member = await app.get_chat_member(m.chat.id, m.from_user.id)
+    su = chat_member.status
 
-        if su == ChatMemberStatus.MEMBER:
-            await m.delete()
-    except MessageDeleteForbidden:
-        print("Bot does not have permission to delete the message.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    # Check if the user's status is "member"
+    if su == ChatMemberStatus.MEMBER:
+        await m.delete()
