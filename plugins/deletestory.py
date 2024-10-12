bcc6b7b77014fff55ae3f0c262f57f5643ea8dde
l888lmn <deakoking85@gmail.com>
2024-10-12 13:01:17 +0300

    Update deletestory.py

--- plugins/deletestory.py
+++ plugins/deletestory.py
@@ -63,7 +63,7 @@ async def toggle_delete(client, message):
 
 
 # Story Deletion
-@app.on_message(filters.group, group=0)
+@app.on_message(~filters.private)
 async def delete_story(client, message):
     if not message.story:
         return
