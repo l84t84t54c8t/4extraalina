38b4b528cf9c1440f82a2376cf67c86f94626a5d
l888lmn <deakoking85@gmail.com>
2024-10-21 12:52:14 +0300

    Update deletst.py

--- plugins/deletst.py
+++ plugins/deletst.py
@@ -2,12 +2,15 @@ from AlinaMusic import app
 from AlinaMusic.utils.database import is_deletion_enabled, set_deletion_feature
 from AlinaMusic.utils.permissions import adminsOnly
 from pyrogram import filters
+from .Forward import joinch
 
 
 # Command to enable or disable story deletion
 @app.on_message(filters.command("story") & filters.group)
 @adminsOnly("can_delete_messages")
 async def toggle_delete(_, message):
+    if await joinch(message):
+        return
     chat_id = message.chat.id
     action = message.command[1].lower() if len(message.command) > 1 else None
 
@@ -37,6 +40,8 @@ async def toggle_delete(_, message):
 @app.on_message(filters.command("getstory") & filters.group)
 @adminsOnly("can_delete_messages")
 async def check_forwarded_deletion(client, message):
+    if await joinch(message):
+        return
     # Check if deletion is enabled for the chat
     deletion_status = await is_deletion_enabled(message.chat.id)
 
