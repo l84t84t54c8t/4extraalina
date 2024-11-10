612097835fcdb71ea0ad6472da9968312cb8272d
l888lmn <deakoking85@gmail.com>
2024-11-10 23:02:47 +0300

    Update join2.py

--- plugins/join2.py
+++ plugins/join2.py
@@ -15,14 +15,11 @@ async def must_join_channel(app: Client, msg: Message):
             await app.get_chat_member(MUST_JOIN2, msg.from_user.id)
         except UserNotParticipant:
             if MUST_JOIN2.isalpha():
-                link = f"https://t.me/{MUST_JOIN2}"
-                chat_info = await app.get_chat(MUST_JOIN2)
-                channel_name = chat_info.title  # Get the channel name
+                link = "https://t.me/" + MUST_JOIN2
             else:
                 chat_info = await app.get_chat(MUST_JOIN2)
                 link = chat_info.invite_link
-                channel_name = chat_info.title  # Get the channel name
-
+                channel_name = chat_info.title
             try:
                 await msg.reply(
                     f"**• Sorry . . {msg.from_user.mention}\n• You must first join group to use me\n• Group : « @{MUST_JOIN2} »\n\n• ببووره . . ئەزیزم {msg.from_user.mention}\n• سەرەتا پێویستە جۆینی گرووپ بکەیت بۆ بەکارهێنانم\n• گرووپ : «  @{MUST_JOIN2} »**",
@@ -30,9 +27,7 @@ async def must_join_channel(app: Client, msg: Message):
                         [
                             [
                                 InlineKeyboardButton(
-                                    # Use the channel name in the button text
-                                    f"{channel_name}",
-                                    url=link,
+                                    channel_name, url=link
                                 ),
                             ]
                         ]
