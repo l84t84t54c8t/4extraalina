7aed7f16d8c9e2fac8712879e551c23cc46f22e9
l888lmn <deakoking85@gmail.com>
2024-11-10 22:32:54 +0300

    Update join1.py

--- plugins/join1.py
+++ plugins/join1.py
@@ -1,49 +1,79 @@
 from AlinaMusic import app
-from config import MUST_JOIN, MUST_JOIN2
+from config import MUST_JOIN, MUST_JOIN2  # Assuming two separate channel vars
 from pyrogram import Client, filters
-from pyrogram.errors import (ChatAdminRequired, ChatWriteForbidden,
-                             UserNotParticipant)
+from pyrogram.errors import (ChatAdminRequired, ChatWriteForbidden, UserNotParticipant)
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 # --------------------------
-# Assuming MUST_JOIN is a list of channel usernames or IDs, like: MUST_JOIN = ["channel1", "channel2"]
-MUST_JOINN = ["MUST_JOIN", "MUST_JOIN2"]
 
+# ------------------------
 @app.on_message(filters.incoming & filters.private, group=-2)
 async def must_join_channel(app: Client, msg: Message):
-    if not MUST_JOINN:
+    if not (MUST_JOIN and MUST_JOIN2):
         return
-    for channel in MUST_JOINN:
+    
+    # Check if user is a member of the first channel
+    try:
+        await app.get_chat_member(MUST_JOIN, msg.from_user.id)
+    except UserNotParticipant:
+        if MUST_JOIN.isalpha():
+            link = f"https://t.me/{MUST_JOIN}"
+        else:
+            chat_info = await app.get_chat(MUST_JOIN)
+            name = chat_info.first_name
+            link = chat_info.invite_link
+        
         try:
-            await app.get_chat_member(channel, msg.from_user.id)
-        except UserNotParticipant:
-            if channel.isalpha():
-                link = f"https://t.me/{channel}"
-            else:
-                chat_info = await app.get_chat(channel)
-                link = chat_info.invite_link
-            try:
-                await msg.reply(
-                    f"**• Sorry, {msg.from_user.mention}.\n"
-                    f"• You must join all required channels to use me.\n"
-                    f"• Please join: @{channel}\n\n"
-                    f"• ببووره {msg.from_user.mention}.\n"
-                    f"• پێویستە ئەم کەناڵەکان جۆین بکەیت بۆ بەکارهێنانم.\n"
-                    f"• کەناڵ : @{channel}**",
-                    reply_markup=InlineKeyboardMarkup(
+            await msg.reply(
+                f"**• Sorry . . {msg.from_user.mention}\n• You must first join channel to use me\n• Channel: « @{MUST_JOIN} »\n\n• ببووره . . ئەزیزم {msg.from_user.mention}\n• سەرەتا پێویستە جۆینی کەناڵ بکەیت بۆ بەکارهێنانم\n• کەناڵ: « @{MUST_JOIN} »**",
+                reply_markup=InlineKeyboardMarkup(
+                    [
                         [
-                            [
-                                InlineKeyboardButton(
-                                    "Join Channel", url=link
-                                ),
-                            ]
+                            InlineKeyboardButton(
+                                name, url=link
+                            ),
                         ]
-                    ),
-                    disable_web_page_preview=True,
-                )
-                await msg.stop_propagation()
-                return  # Stop checking after first non-member channel
-            except ChatWriteForbidden:
-                pass
+                    ]
+                ),
+                disable_web_page_preview=True,
+            )
+            await msg.stop_propagation()
+        except ChatWriteForbidden:
+            pass
+        return  # Stop here if the user is not in the first channel
+    
+    # Check if user is a member of the second channel
+    try:
+        await app.get_chat_member(MUST_JOIN2, msg.from_user.id)
+    except UserNotParticipant:
+        if MUST_JOIN2.isalpha():
+            link = f"https://t.me/{MUST_JOIN2}"
+        else:
+            chat_info = await app.get_chat(MUST_JOIN2)
+            name = chat_info.first_name
+            link = chat_info.invite_link
+        
+        try:
+            await msg.reply(
+                f"**• Sorry . . {msg.from_user.mention}\n• You must first join channel to use me\n• Channel: « @{MUST_JOIN2} »\n\n• ببووره . . ئەزیزم {msg.from_user.mention}\n• سەرەتا پێویستە جۆینی کەناڵ بکەیت بۆ بەکارهێنانم\n• کەناڵ: « @{MUST_JOIN2} »**",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton(
+                                name, url=link
+                            ),
+                        ]
+                    ]
+                ),
+                disable_web_page_preview=True,
+            )
+            await msg.stop_propagation()
+        except ChatWriteForbidden:
+            pass
+        return  # Stop here if the user is not in the second channel
+    
+    # If user is in either of the channels, proceed normally
+    pass
+
     except ChatAdminRequired:
-        print(f"**Bot needs admin permissions in the channel**: {channel}!")
+        print(f"**بۆت بکە ئەدمین لە کەناڵی**: {MUST_JOIN_1} or {MUST_JOIN_2} !")
