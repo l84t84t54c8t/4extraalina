8ba09fe9686ff71135a086755eb91b3290c19f74
l888lmn <deakoking85@gmail.com>
2024-11-10 22:58:43 +0300

    Update join2.py

--- plugins/join2.py
+++ plugins/join2.py
@@ -1,10 +1,8 @@
-from AlinaMusic import app
 from config import MUST_JOIN2
 from pyrogram import Client, filters
-from pyrogram.errors import (ChatAdminRequired, ChatWriteForbidden,
-                             UserNotParticipant)
+from pyrogram.errors import ChatAdminRequired, ChatWriteForbidden, UserNotParticipant
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-
+from AlinaMusic import app
 
 @app.on_message(filters.incoming & filters.private, group=-2)
 async def must_join_channel(app: Client, msg: Message):
@@ -14,23 +12,24 @@ async def must_join_channel(app: Client, msg: Message):
         try:
             await app.get_chat_member(MUST_JOIN2, msg.from_user.id)
         except UserNotParticipant:
-            try:
-                if MUST_JOIN2.isalpha():
-                    link = f"https://t.me/{MUST_JOIN2}"
-                    chat_info = await app.get_chat(MUST_JOIN2)
-                    name = chat_info.title
-                else:
-                    chat_info = await app.get_chat(MUST_JOIN2)
-                    link = chat_info.invite_link
-                    name = chat_info.title
+            if MUST_JOIN2.isalpha():
+                link = f"https://t.me/{MUST_JOIN2}"
+                chat_info = await app.get_chat(MUST_JOIN2)
+                channel_name = chat_info.title  # Get the channel name
+            else:
+                chat_info = await app.get_chat(MUST_JOIN2)
+                link = chat_info.invite_link
+                channel_name = chat_info.title  # Get the channel name
 
-                try:
-                    await msg.reply(
-                    f"**• Sorry . . {msg.from_user.mention}\n• You must first join the group to use me\n• Group: « @{MUST_JOIN2} »\n\n• ببووره . . ئەزیزم {msg.from_user.mention}\n• سەرەتا پێویستە جۆینی گرووپ بکەیت بۆ بەکارهێنانم\n• گرووپ : « @{MUST_JOIN2} »**",
+            try:
+                await msg.reply(
+                    f"**• Sorry . . {msg.from_user.mention}\n• You must first join group to use me\n• Group : « @{MUST_JOIN2} »\n\n• ببووره . . ئەزیزم {msg.from_user.mention}\n• سەرەتا پێویستە جۆینی گرووپ بکەیت بۆ بەکارهێنانم\n• گرووپ : «  @{MUST_JOIN2} »**",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton(name, url=link),
+                                InlineKeyboardButton(
+                                    f"{channel_name}", url=link  # Use the channel name in the button text
+                                ),
                             ]
                         ]
                     ),
@@ -41,5 +40,3 @@ async def must_join_channel(app: Client, msg: Message):
                 pass
     except ChatAdminRequired:
         print(f"**بۆت بکە ئەدمین لە کەناڵی**: {MUST_JOIN2} !")
-    except KeyError as e:
-        print(f"Username not found: {e}")
