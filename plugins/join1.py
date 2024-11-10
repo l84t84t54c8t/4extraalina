ac10bd0ba2892674f913e2d27e819c8e2fafa135
l888lmn <deakoking85@gmail.com>
2024-11-10 23:02:50 +0300

    Update join1.py

--- plugins/join1.py
+++ plugins/join1.py
@@ -15,24 +15,19 @@ async def must_join_channel(app: Client, msg: Message):
             await app.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
             if MUST_JOIN.isalpha():
-                link = f"https://t.me/{MUST_JOIN}"
-                chat_info = await app.get_chat(MUST_JOIN)
-                channel_name = chat_info.title  # Get the channel name
+                link = "https://t.me/" + MUST_JOIN
             else:
                 chat_info = await app.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
-                channel_name = chat_info.title  # Get the channel name
-
+                channel_name = chat_info.title
             try:
                 await msg.reply(
-                    f"**• Sorry . . {msg.from_user.mention}\n• You must first join channel to use me\n• Channel : « @{MUST_JOIN} »\n\n• ببووره . . ئەزیزم {msg.from_user.mention}\n• سەرەتا پێویستە جۆینی کەناڵ بکەیت بۆ بەکارهێنانم\n• کەناڵ : «  @{MUST_JOIN} »**",
+                    f"**• Sorry . . {msg.from_user.mention}\n• You must first join channel to use me\n• Channel : « @{MUST_JOIN} »\n\n• ببووره . . ئەزیزم {msg.from_user.mention}\n• سەرەتا پێویستە جۆینی کەناڵ بکەیت بۆ بەکارهێنانم\n• کەناڵ : « @{MUST_JOIN} »**",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
                                 InlineKeyboardButton(
-                                    # Use the channel name in the button text
-                                    f"{channel_name}",
-                                    url=link,
+                                    channel_name, url=link
                                 ),
                             ]
                         ]
