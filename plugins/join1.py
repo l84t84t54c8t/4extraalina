5c3dae46ad8f6cf88d6329629ca626c32c9e7d0b
l888lmn <deakoking85@gmail.com>
2024-11-10 19:37:34 +0300

    Update join1.py

--- plugins/join1.py
+++ plugins/join1.py
@@ -1,35 +1,41 @@
 from AlinaMusic import app
-from config import MUST_JOIN
+from config import MUST_JOIN, MUST_JOIN2
 from pyrogram import Client, filters
 from pyrogram.errors import (ChatAdminRequired, ChatWriteForbidden,
                              UserNotParticipant)
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 # --------------------------
+# Assuming MUST_JOIN is a list of channel usernames or IDs, like: MUST_JOIN = ["channel1", "channel2"]
+MUST_JOINN = ["MUST_JOIN", "MUST_JOIN2"]
 
-
-# ------------------------
 @app.on_message(filters.incoming & filters.private, group=-2)
 async def must_join_channel(app: Client, msg: Message):
-    if not MUST_JOIN:
+    if not MUST_JOINN:
         return
-    try:
+    for channel in MUST_JOINN:
         try:
-            await app.get_chat_member(MUST_JOIN, msg.from_user.id)
+            await app.get_chat_member(channel, msg.from_user.id)
         except UserNotParticipant:
-            if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
+            if channel.isalpha():
+                link = f"https://t.me/{channel}"
             else:
-                chat_info = await app.get_chat(MUST_JOIN)
+                chat_info = await app.get_chat(channel)
+                name = chat_info.first_name
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"**• Sorry . . {msg.from_user.mention}\n• You must first join channel to use me\n• Channel : « @{MUST_JOIN} »\n\n• ببووره . . ئەزیزم {msg.from_user.mention}\n• سەرەتا پێویستە جۆینی کەناڵ بکەیت بۆ بەکارهێنانم\n• کەناڵ : « @{MUST_JOIN} »**",
+                    f"**• Sorry, {msg.from_user.mention}.\n"
+                    f"• You must join all required channels to use me.\n"
+                    f"• Please join: @{channel}\n\n"
+                    f"• ببووره {msg.from_user.mention}.\n"
+                    f"• پێویستە ئەم کەناڵەکان جۆین بکەیت بۆ بەکارهێنانم.\n"
+                    f"• کەناڵ : @{channel}**",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
                                 InlineKeyboardButton(
-                                    "˼ احساس ˹", url=f"https://t.me/{MUST_JOIN}"
+                                    name, url=link
                                 ),
                             ]
                         ]
@@ -37,7 +43,8 @@ async def must_join_channel(app: Client, msg: Message):
                     disable_web_page_preview=True,
                 )
                 await msg.stop_propagation()
+                return  # Stop checking after first non-member channel
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"**بۆت بکە ئەدمین لە کەناڵی**: {MUST_JOIN} !")
+        print(f"****بۆت بکە ئەدمین لە کەناڵی**: {MUST_JOINN}!")
