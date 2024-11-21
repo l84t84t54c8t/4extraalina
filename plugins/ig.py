3d6dc942a451285764f9cfd7dd4a8667e27c9636
l888lmn <deakoking85@gmail.com>
2024-11-21 20:21:54 +0300

    Update ig.py

--- plugins/ig.py
+++ plugins/ig.py
@@ -2,51 +2,32 @@ import re
 
 import requests
 from AlinaMusic import app
-from config import SUPPORT_CHANNEL
-from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import requests
+from pyrogram import Client, filters
 
 # Regex pattern to match Instagram URLs
 instagram_url_pattern = r"(https?://(?:www\.)?instagram\.com/[-a-zA-Z0-9@:%._\+~#=]{2,256}/[-a-zA-Z0-9@:%._\+~#=]+)"
 
-
 @app.on_message(filters.regex(instagram_url_pattern))
-async def download_instagram_video(app, message):
+async def down(app, message):
     try:
-        # Extract the Instagram link from the message
-        link = re.search(instagram_url_pattern, message.text).group(1)
-
-        # Make a request to fetch the video
-        json_data = {"url": link}
-        response = requests.post(
-            "https://insta.savetube.me/downloadPostVideo", json=json_data
-        ).json()
+        link = message.text
+        json_data = {'url': link}
+        response = requests.post('https://insta.savetube.me/downloadPostVideo', json=json_data).json()
 
         # Extract video and thumbnail
-        thu = response["post_video_thumbnail"]
-        video = response["post_video_url"]
+        thu = response['post_video_thumbnail']
+        video = response['post_video_url']
 
         # Send thumbnail as a photo
-        await message.reply_text("**← کەمێک چاوەڕێ بکە .. ڤیدیۆ دادەبەزێت ...**")
+        await message.reply_photo(thu, caption="**← کەمێک چاوەڕێ بکە .. ڤیدیۆ دادەبەزێت ...\n⧉• لەلایەن : @IQMCBOT**")
 
         # Send video directly
-        caption = "**✅ ꒐ بە سەرکەوتوویی داگرترا\n🎸 ꒐ @IQMCBOT**"
-        await app.send_video(
-            message.chat.id,
-            video,
-            caption=caption,
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            text=_["S_B_4"],
-                            url=f"{SUPPORT_CHANNEL}",
-                        )
-                    ]
-                ]
-            ),
+        caption = (
+            "**✅꒐ بە سەرکەوتوویی داگرترا\n🎸꒐ بۆتی @IQMCBOT**"
         )
+        await app.send_video(message.chat.id, video, caption=caption)
 
     except Exception as e:
         print(f"Error: {e}")
-        await message.reply("**ببورە، هەڵەیەک ڕوویدا! تکایە دوبارە هەوڵدەوە.**")
+        await message.reply("**لینك هەڵەیە ئەزیزم**")
