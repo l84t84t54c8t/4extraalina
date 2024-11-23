a4df349b0c28bf4de97f1003462dc6b7ba199511
l888lmn <deakoking85@gmail.com>
2024-11-23 22:48:59 +0300

    Update massaction.py

--- plugins/massaction.py
+++ plugins/massaction.py
@@ -15,8 +15,8 @@ def get_keyboard(command):
     return InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("Yes", callback_data=f"{command}_yes"),
-                InlineKeyboardButton("No", callback_data=f"{command}_no"),
+                InlineKeyboardButton("بەڵێ ✅", callback_data=f"{command}_yes"),
+                InlineKeyboardButton("نەخێر ❌", callback_data=f"{command}_no"),
             ]
         ]
     )
@@ -32,27 +32,24 @@ async def banall(client: Client, message: Message):
     assid = ass.id
 
     AMBOTOK = await message.reply_text(
-        f"Hey {message.from_user.mention}, 'deleteall' checking..."
+        f"**بەڕێز {message.from_user.mention}\nپشکنینی گرووپ بۆ سڕینەوەی نامەکان**"
     )
     await asyncio.sleep(2)
     bot = await app.get_chat_member(chat_id, app.me.id)
     if not (
         bot.privileges.can_delete_messages
-        and bot.privileges.can_promote_members
-        and bot.privileges.can_invite_users
     ):
         await AMBOTOK.edit(
-            f"I don't have enough permissions to Mass Action !?!\n"
-            f"Mass Action requires the bot permissions to Run properly Mass Action.\n"
-            f"1.) delete messages.\n"
-            f"2.) promote members.\n"
-            f"3.) invite users."
+            f"**من ڕۆڵی پێویستم نییە بۆ ئەنجامدانی\n**"
+            f"**پێویستی بە ڕۆڵی.\n**"
+            f"**- سڕینەوەی نامە.\n**"
+            f"**- دەرکردن و میوت کردن.\n**"
         )
         return
     await asyncio.sleep(5)
     await AMBOTOK.delete()
     confirm_msg = await message.reply(
-        f"{message.from_user.mention}, are you sure you want to delete all group messages?",
+        f"**بەڕێز {message.from_user.mention}\nدڵنیایی، دەتەوێ هەموو نامەکان بسڕمەوە ؟**",
         reply_markup=get_keyboard("deleteall"),
     )
 
@@ -73,12 +70,12 @@ async def handle_callback(client: Client, callback_query: CallbackQuery):
             owner_AMBOT = admin.user.mention
     if user_id != owner_id and user_id not in SUDOERS:
         await callback_query.answer(
-            "Only the group owner can confirm this action.", show_alert=True
+            "**تەنها خاوەنی گرووپ دەتوانێت ئەم کارە بکات.**", show_alert=True
         )
         return
 
     if callback_query.data == "deleteall_yes":
-        await callback_query.answer("Delete all process started...", show_alert=True)
+        await callback_query.answer("**سڕینەوەی نامەکان دەستی پێکرد . .**", show_alert=True)
         await app.promote_chat_member(
             chat_id,
             assid,
@@ -116,111 +113,15 @@ async def handle_callback(client: Client, callback_query: CallbackQuery):
                 ),
             )
             await callback_query.answer(
-                "All messages deleted successfully.", show_alert=False
+                "**بە سەرکەوتوویی هەموو نامەکان سڕدرانەوە.**", show_alert=False
             )
         except Exception as e:
             await callback_query.answer(
-                f"An error occurred: {str(e)}", show_alert=False
+                f"**هەڵە:\n{str(e)}**", show_alert=False
             )
     elif callback_query.data == "deleteall_no":
-        await callback_query.message.edit("Delete all process canceled.")
+        await callback_query.message.edit("**سڕینەوەی نامەکان هەڵوەشێنرایەوە.**")
 
-
-@app.on_message(filters.command("abanall") & SUDOERS)
-async def ban_all(client: Client, message: Message):
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    userbot = await get_assistant(message.chat.id)
-    # Get group owner
-
-    bot_member = await client.get_chat_member(chat_id, userbot.id)
-    if not bot_member.privileges or not bot_member.privileges.can_restrict_members:
-        try:
-            await app.promote_chat_member(
-                chat_id,
-                assid,
-                privileges=ChatPrivileges(
-                    can_manage_chat=False,
-                    can_delete_messages=False,
-                    can_manage_video_chats=False,
-                    can_restrict_members=True,
-                    can_change_info=False,
-                    can_invite_users=False,
-                    can_pin_messages=False,
-                    can_promote_members=True,
-                ),
-            )
-        except ChatAdminRequired:
-            await message.reply("I need admin rights to ban members.")
-            return
-
-    # Ban all members
-    banned_count = 0
-    async for member in client.get_chat_members(chat_id):
-        if member.status in [
-            ChatMemberStatus.ADMINISTRATOR,
-            ChatMemberStatus.OWNER,
-        ] or member.user.id in [userbot.id, user_id]:
-            continue
-        try:
-            await userbot.ban_chat_member(chat_id, member.user.id)
-            banned_count += 1
-        except Exception as e:
-            print(f"Failed to ban {member.user.id}: {e}")
-
-    await message.reply(f"Banned {banned_count} members successfully.")
-
-
-"""
-@app.on_message(filters.command("unbanall"))
-async def unbanall(client: Client, message: Message):
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    owner_id = None
-    async for admin in client.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
-        if admin.status == enums.ChatMemberStatus.OWNER:
-            owner_id = admin.user.id
-            owner_AMBOT = admin.user.mention
-    if user_id != owner_id and user_id not in SUDOERS:
-        await message.reply_text(f"Hey {message.from_user.mention}, 'unbanall' can only be executed by the group owner {owner_AMBOT}.")
-        return
-    confirm_msg = await message.reply(
-        f"{message.from_user.mention}, are you sure you want to unban all group members?",
-        reply_markup=get_keyboard("unbanall")
-    )
-
-@app.on_callback_query(filters.regex(r"^unbanall_(yes|no)$"))
-async def handle_unbanall_callback(client: Client, callback_query: CallbackQuery):
-    chat_id = callback_query.message.chat.id
-    user_id = callback_query.from_user.id
-    owner_id = None
-    async for admin in client.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
-        if admin.status == enums.ChatMemberStatus.OWNER:
-            owner_id = admin.user.id
-            owner_AMBOT = admin.user.mention
-    if user_id != owner_id and user_id not in SUDOERS:
-        await callback_query.answer("Only the group owner can confirm this action.", show_alert=True)
-        return
-    if callback_query.data == "unbanall_yes":
-        await callback_query.message.edit("Unbanall process started...")
-        bot = await app.get_chat_member(chat_id, app.me.id)
-        banned_users = []
-        unbanned = 0
-        if not bot.privileges.can_restrict_members:
-            await callback_query.message.edit("I don't have permission to unban members in this group.")
-            return
-        async for member in app.get_chat_members(chat_id, filter=enums.ChatMembersFilter.BANNED):
-            banned_users.append(member.user.id)
-            try:
-                await app.unban_chat_member(chat_id, banned_users[-1])
-                unbanned += 1
-            except Exception as e:
-                print(f"Failed to unban {member.user.id}: {e}")
-        await callback_query.message.edit(f"Unbanned {unbanned} members successfully.")
-    elif callback_query.data == "unbanall_no":
-        await callback_query.message.edit("Unbanall process canceled.")
-
-"""
 
 
 @app.on_message(filters.command("unmutealll"))
@@ -236,12 +137,12 @@ async def unmuteall(client: Client, message: Message):
             owner_AMBOT = admin.user.mention
     if user_id != owner_id and user_id not in SUDOERS:
         await message.reply_text(
-            f"Hey {message.from_user.mention}, 'unmuteall' can only be executed by the group owner {owner_AMBOT}."
+            f"**بەڕێز {message.from_user.mention}\n**تەنها خاوەنی گرووپ {owner_AMBOT} دەتوانێت بیکات.**"
         )
         return
 
     confirm_msg = await message.reply(
-        f"{message.from_user.mention}, are you sure you want to unmute all group members?",
+        f"**بەڕێز {message.from_user.mention}\nدڵنیایی، دەتەوێ میوتی هەموو ئەندامەکانی گرووپ لابدەیت ؟**",
         reply_markup=get_keyboard("unmuteall"),
     )
 
@@ -263,16 +164,16 @@ async def handle_unmuteall_callback(client: Client, callback_query: CallbackQuer
         )
         return
     if callback_query.data == "unmuteall_yes":
-        await callback_query.message.edit("Unmuteall process started...")
+        await callback_query.message.edit("لادانی میوتی گشتی ...")
         bot = await app.get_chat_member(chat_id, app.me.id)
         if not bot.privileges.can_restrict_members:
             await callback_query.message.edit(
-                "I don't have permission to restrict members in this group."
+                "ببورە من ڕۆڵی میوت و دەرکردنم نییە."
             )
             return
         unmuted = 0
         async for member in app.get_chat_members(chat_id):
-            if member.status in ["administrator", "creator"]:
+            if member.status in [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]:
                 continue
             try:
                 await app.restrict_chat_member(
@@ -289,9 +190,9 @@ async def handle_unmuteall_callback(client: Client, callback_query: CallbackQuer
                 unmuted += 1
             except Exception as e:
                 print(f"Failed to unmute {member.user.id}: {e}")
-        await callback_query.message.edit(f"Unmuted {unmuted} members successfully.")
+        await callback_query.message.edit(f"**بە سەرکەوتوویی {unmuted} میوتی لادرا.**")
     elif callback_query.data == "unmuteall_no":
-        await callback_query.message.edit("Unmuteall process canceled.")
+        await callback_query.message.edit("**لادانی میوتی گشتی هەڵوەشێنرایەوە.**")
 
 
 @app.on_message(filters.command("mutealll"))
@@ -307,11 +208,11 @@ async def muteall(client: Client, message: Message):
             owner_AMBOT = admin.user.mention
     if user_id != owner_id and user_id not in SUDOERS:
         await message.reply_text(
-            f"Hey {message.from_user.mention}, 'muteall' can only be executed by the group owner {owner_AMBOT}."
+            f"**بەڕێز {message.from_user.mention}\n**تەنها خاوەنی گرووپ {owner_AMBOT} دەتوانێت بیکات.**"
         )
         return
     confirm_msg = await message.reply(
-        f"{message.from_user.mention}, are you sure you want to mute all group members?",
+        f"**بەڕێز {message.from_user.mention}\nدڵنیایی، دەتەوێ هەموو ئەندامەکانی گرووپ میوت بکەیت ؟**",
         reply_markup=get_keyboard("muteall"),
     )
 
@@ -329,20 +230,20 @@ async def handle_muteall_callback(client: Client, callback_query: CallbackQuery)
             owner_AMBOT = admin.user.mention
     if user_id != owner_id and user_id not in SUDOERS:
         await callback_query.answer(
-            "Only the group owner can confirm this action.", show_alert=True
+            "تەنها خاوەنی گرووپ دەتوانێت ئەم کارە بکات.", show_alert=True
         )
         return
     if callback_query.data == "muteall_yes":
-        await callback_query.message.edit("Muteall process started...")
+        await callback_query.message.edit("میوت کردنی هەموو ئەندامەکان ...")
         bot = await app.get_chat_member(chat_id, app.me.id)
         if not bot.privileges.can_restrict_members:
             await callback_query.message.edit(
-                "I don't have permission to restrict members in this group."
+                "ببورە من ڕۆڵی میوت و دەرکردنم نییە."
             )
             return
         muted = 0
         async for member in app.get_chat_members(chat_id):
-            if member.status in ["administrator", "creator"]:
+            if member.status in [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]:
                 continue
             try:
                 await app.restrict_chat_member(
@@ -351,12 +252,12 @@ async def handle_muteall_callback(client: Client, callback_query: CallbackQuery)
                 muted += 1
             except Exception as e:
                 print(f"Failed to mute {member.user.id}: {e}")
-        await callback_query.message.edit(f"Muted {muted} members successfully.")
+        await callback_query.message.edit(f"**بە سەرکەوتوویی {muted} ئەندام میوت کرا.**")
     elif callback_query.data == "muteall_no":
-        await callback_query.message.edit("Muteall process canceled.")
+        await callback_query.message.edit("**میوتی گشتی هەڵوەشێنرایەوە.**")
 
 
-@app.on_message(filters.command("kickalll"))
+@app.on_message(filters.command("kickalllll"))
 async def kickall(client: Client, message: Message):
     chat_id = message.chat.id
     user_id = message.from_user.id
@@ -419,7 +320,7 @@ async def handle_kickall_callback(client: Client, callback_query: CallbackQuery)
         await callback_query.message.edit("Kickall process canceled.")
 
 
-@app.on_message(filters.command("unpinall"))
+@app.on_message(filters.command("unpinalll"))
 async def unpinall(client: Client, message: Message):
     chat_id = message.chat.id
     user_id = message.from_user.id
