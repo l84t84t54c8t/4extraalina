4ad9353986062bbcddd76ee5a016e05f94716d09
l888lmn <deakoking85@gmail.com>
2024-12-08 20:15:56 +0300

    Update couple.py

--- plugins/couple.py
+++ plugins/couple.py
@@ -8,6 +8,23 @@ from pyrogram import filters
 from pyrogram.enums import ChatAction, ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+from utils.permissions import adminsOnly
+# Lock state variable (you can also use a database)
+command_locked = False
+
+@app.on_message(filters.command("lock_couples") & ~BANNED_USERS)
+@adminsOnly("can_change_info")
+async def lock_couples_command(app, message):
+    global command_locked
+    command_locked = True
+    await message.reply_text("The 'couples' command has been locked! 🔒")
+
+@app.on_message(filters.command("unlock_couples") & ~BANNED_USERS)
+@adminsOnly("can_change_info")
+async def unlock_couples_command(app, message):
+    global command_locked
+    command_locked = False
+    await message.reply_text("The 'couples' command has been unlocked! 🔓")
 
 @app.on_message(
     filters.command(
@@ -17,21 +34,29 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
     & ~BANNED_USERS
 )
 async def couples(app, message):
+    global command_locked
+    if command_locked:
+        return await message.reply_text("This command is currently locked! 🔒")
+
     cid = message.chat.id
     if message.chat.type == ChatType.PRIVATE:
         return await message.reply_text("**تەنیا لە گرووپ کارەکات😂🙂**")
+
     try:
         msg = await message.reply_text("**دوو ئاشقە شێتەکە دیاری دەکرێت😂🙂🫶🏻!**")
         list_of_users = []
 
-        async for i in app.get_chat_members(message.chat.id, limit=50):
-            if not i.user.is_bot and not i.user.is_deleted:
-                list_of_users.append(i.user.id)
+        async for member in app.get_chat_members(message.chat.id, limit=50):
+            if not member.user.is_bot and not member.user.is_deleted:
+                list_of_users.append(member.user.id)
+
+        if len(list_of_users) < 2:
+            return await msg.edit("Not enough members to form a couple! 😢")
 
         c1_id = random.choice(list_of_users)
         c2_id = random.choice(list_of_users)
         while c1_id == c2_id:
-            c1_id = random.choice(list_of_users)
+            c2_id = random.choice(list_of_users)
 
         photo1 = (await app.get_chat(c1_id)).photo
         photo2 = (await app.get_chat(c2_id)).photo
@@ -47,15 +72,11 @@ async def couples(app, message):
             p2 = await app.download_media(photo2.big_file_id, file_name="pfp1.png")
         except Exception:
             p2 = "assets/upic.png"
-        try:
-            await app.resolve_peer(OWNER_ID[0])
-            OWNER = OWNER_ID[0]
-        except BaseException:
-            OWNER = f"tg://openmessage?user_id={OWNER_ID[0]}"
 
-        img1 = Image.open(f"{p1}")
-        img2 = Image.open(f"{p2}")
+        OWNER = OWNER_ID[0] if isinstance(OWNER_ID, list) else OWNER_ID
 
+        img1 = Image.open(p1)
+        img2 = Image.open(p2)
         img = Image.open("assets/cppic.png")
 
         img1 = img1.resize((437, 437))
@@ -71,8 +92,6 @@ async def couples(app, message):
 
         img1.putalpha(mask)
         img2.putalpha(mask1)
-
-        draw = ImageDraw.Draw(img)
 
         img.paste(img1, (116, 160), img1)
         img.paste(img2, (789, 160), img2)
@@ -96,17 +115,15 @@ async def couples(app, message):
             ),
         )
         await msg.delete()
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        # Cleanup
         try:
-            os.remove(f"./downloads/pfp1.png")
-            os.remove(f"./downloads/pfp2.png")
+            os.remove("pfp.png")
+            os.remove("pfp1.png")
             os.remove(f"test_{cid}.png")
         except Exception:
             pass
-    except Exception as e:
-        print(str(e))
-    try:
-        os.remove(f"./downloads/pfp1.png")
-        os.remove(f"./downloads/pfp2.png")
-        os.remove(f"test_{cid}.png")
-    except Exception:
-        pass
